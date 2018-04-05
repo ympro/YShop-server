a71ca5d570b53c91db397b673ae8be3843432ddb
yanmeng <jnyanmeng@163.com>
2018-04-05 18:11:00 +0800

    fix project structure

--- yshop-cloud/yshop-cloud-eureka/src/main/java/com/yshop/cloud/eureka/Application.java
+++ yshop-cloud/yshop-cloud-eureka/src/main/java/com/yshop/cloud/eureka/Application.java
@@ -2,6 +2,7 @@ package com.yshop.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Yan Meng
