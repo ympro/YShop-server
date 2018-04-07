23dac59f86d9142dd842a19e5a795f8e6b1e1698
yanmeng <jnyanmeng@163.com>
2018-04-07 16:13:18 +0800

    add product module code

--- yshop-service/yshop-service-product/src/main/java/com/yshop/service/product/ProductApplication.java
+++ yshop-service/yshop-service-product/src/main/java/com/yshop/service/product/ProductApplication.java
@@ -1,8 +1,16 @@
 package com.yshop.service.product;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Yan Meng
  * @date 2018/4/7.
  */
+@SpringBootApplication
 public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
 }
